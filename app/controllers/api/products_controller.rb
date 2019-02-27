0abf9ae952f62e6dad0fdcd96650f2fc6e0cfdc0
Justin Peebles <justin1peebles@gmail.com>
2019-02-27 15:21:56 -0600

    added action to show first product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,8 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jbuilder"
   end
+  def first_product_method
+    @product = Product.first
+    render "first_product.json.jbuilder"
+  end
 end
