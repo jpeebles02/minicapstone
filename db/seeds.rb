5290021b702ca6f54935254e1fdd353d8edf241f
Justin Peebles <justin1peebles@gmail.com>
2019-02-27 14:34:22 -0600

    added products to seeds.rd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create({name: "Basketball", price: 10, description: "Orange Ball with Black Lining"}. rating: "5 star"})
+Product.create({name: "Football", price: 8, description: "Brown Ball with White Stitches", rating: "4 star"})
+Product.create({name: "Soccer Ball", price: 10, description: "Black and White Spotted Ball", rating: "5 star"})
+Product.create({name: "Tennis Ball", price: 5, description: "Green Ball", rating: "3 star"})
+Product.create({name: "Baseball", price: 5, description: "White Ball with Red Stitches", rating: })
